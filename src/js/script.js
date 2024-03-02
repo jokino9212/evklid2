74fc5226889c5e9abeb79c112b1d6b0f97c730fb
Dmitry Pavlenko <104529636+jokino9212@users.noreply.github.com>
2024-03-02 13:27:31 +0300

    fix style

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,4 @@ window.addEventListener('DOMContentLoaded', function () {
     document.querySelector('#burger').addEventListener('click', function () {
         document.querySelector('#nav').classList.toggle('nav__is-active')
     })
-
-
-
-    document.querySelectorAll('.title').forEach(function(el) {
-        el.addEventListener('click', function(event) {
-            event.target.classList.toggle('alert')
-        })
-})
 })
